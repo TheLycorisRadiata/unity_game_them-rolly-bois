9d64ca04842ceff5cafd131060d4292ccc36e090
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2022-12-13 14:38:14 +0100

    Generate the room

--- Assets/Scripts/GameInit.cs
+++ Assets/Scripts/GameInit.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class GameInit : MonoBehaviour
 {
     private static AudioManager audioManager;
+    private static GameObject[] arrRooms;
+    [SerializeField]
+    private GameObject roomPrefab;
 
     void Awake()
     {
+        int i, nbrRooms = 1;
+
         audioManager = FindObjectOfType<AudioManager>();
+
+        arrRooms = new GameObject[nbrRooms];
+        for (i = 0; i < arrRooms.Length; ++i)
+            arrRooms[i] = Instantiate(roomPrefab) as GameObject;
     }
 
     void Start()
