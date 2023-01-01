b7d5149523ba0aaeffacd2b2c6e9680b0118aff7
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2023-01-01 19:23:44 +0100

    Jumper

---
+++ Assets/Scripts/Levels/Jumper.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    private static float bounceAmount;
+    private bool isActivated;
+    private Renderer rendererComponent;
+    [SerializeField] private Material activatedMaterial, deactivatedMaterial;
+
+    void Awake()
+    {
+        rendererComponent = GetComponent<Renderer>();
+    }
+
+    void Start()
+    {
+        bounceAmount = 10f;
+        isActivated = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject otherGo = other.gameObject;
+        Rigidbody otherRb;
+
+        if (isActivated && otherGo.CompareTag("Player"))
+        {
+            otherRb = otherGo.GetComponent<Rigidbody>();
+            if (otherRb != null)
+                otherRb.velocity = new Vector3(0f, bounceAmount, 0f);
+        }
+    }
+
+    // Unused for now
+    public void SwitchActivation()
+    {
+        isActivated = !isActivated;
+
+        if (isActivated)
+            rendererComponent.material = activatedMaterial;
+        else
+            rendererComponent.material = deactivatedMaterial;
+    }
+}
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Roll the ball and collect all the coins in this Unity game!
 
 The priority is the extended version: https://www.youtube.com/playlist?list=PLNoRsahkiuzWgIAVbK9AD1kWtI_8i9s8K
 ---
-- Implement a button, anything, to be able to switch the bumper's activation.
+- Implement a button, anything, to be able to switch the bumper/jumper's activation.
 - I want the twin feature: when stepping into some zone, the player duplicates, you now control two balls at once. And when you step into another zone, you go back to being singular. Oh, ok, you know what? Whenever a ball steps into the thingy, it duplicates, so you can find yourself with lots of balls. Of course, you can also merge until you find yourself singular again.
 - Any ball can be destroyed, and I want a small explosion (Synty Prototype Pack).
 - When a ball is destroyed, the coins it gathered are lost as well.
